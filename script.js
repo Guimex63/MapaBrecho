3efacc3eda57cb985fa86c434bfce4c2bd506bc7
Guimex63 <guimex63@gmail.com>
2025-07-29 20:59:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document.getElementById('formulario').addEventListener('submit', function (e) {
+document.getElementById("brechoForm").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert('Brechó cadastrado com sucesso!');
-  this.reset();
+
+  const form = e.target;
+  const nome = form.nome.value;
+  const endereco = form.endereco.value;
+  const cidade = form.cidade.value;
+  const faixaMin = form.preco_min.value;
+  const faixaMax = form.preco_max.value;
+
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <strong>${nome}</strong><br/>
+    Endereço: ${endereco} - ${cidade}<br/>
+    Faixa de preço: R$ ${faixaMin} até R$ ${faixaMax}
+  `;
+
+  document.getElementById("brechoList").appendChild(li);
+  form.reset();
 });
